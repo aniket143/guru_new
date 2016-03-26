45b6dc2bd6f020b39a0b8c5331da14e30bb9e3c9
aniket143 <aniket143@gmail.com>
2016-03-26 14:49:17 +0530

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -6,15 +6,19 @@ var resultDiv;
 
 document.addEventListener("deviceready", init, false);
 function init() {
+	alert("init");
 	document.querySelector("#startScan").addEventListener("click", startScan, false);
 	resultDiv = document.querySelector("#results");
 }
 
 function startScan() {
+	alert("startscan");
 
 //	cordova.plugins.barcodeScanner.scan(
 cordova.plugins.barcodeScanner.scan(
 		function (result) {
+				alert("startscan2");
+
 			var s = "Result: " + result.text + "<br/>" +
 			"Format: " + result.format + "<br/>" +
 			"Cancelled: " + result.cancelled;
